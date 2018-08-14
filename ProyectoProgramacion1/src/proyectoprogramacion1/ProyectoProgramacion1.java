cc6a6a80562c7cbaf5271a06b39e8fbf22baf4ca
ZanoniFranco <fzanoni20@gmail.com>
2018-08-14 11:25:49 -0300

    3.0

--- ProyectoProgramacion1/src/proyectoprogramacion1/ProyectoProgramacion1.java
+++ ProyectoProgramacion1/src/proyectoprogramacion1/ProyectoProgramacion1.java
@@ -4,11 +4,16 @@ import java.awt.*;
 //import java.awt.Color;
 //import java.awt.Font;
 import java.awt.event.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 //import java.awt.event.ActionEvent;
 //import java.awt.event.ActionListener;
 //import java.awt.event.KeyAdapter;
 //import java.awt.event.KeyEvent;
 import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.text.DefaultEditorKit;
 //import javax.swing.JButton;
 //import javax.swing.JCheckBox;
 //import javax.swing.JComboBox;
@@ -19,96 +24,165 @@ import javax.swing.*;
 //import javax.swing.JTextArea;
 //import javax.swing.JTextField;
 
-public class ProyectoProgramacion1 {
+public class ProyectoProgramacion1 extends JFrame{
+    private JTextArea area = new JTextArea(20,120);
+    private JFileChooser dialog = new JFileChooser(System.getProperty("user.dir"));
+    private String currentFile = "Untitled";
+    private boolean changed = false;
+    
+    
     public ProyectoProgramacion1(){
     
-    //FRAME
-        JFrame frame = new JFrame("Text Editor");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLayout(null);
-
-//TEXT AREA
-        JTextArea txtarea = new JTextArea();
-        frame.add(txtarea);
-        //(los bounds del txtxarea dependen de los bounds del scrollpane)
-        //txtarea.setBounds(5, 3, 260, 50); 
-        txtarea.setToolTipText("Hoja");
-        txtarea.setEditable(true);
-        txtarea.setBackground(Color.WHITE);
-
-//SCROLL PANE
-        JScrollPane scroll = new JScrollPane(txtarea);
-        frame.add(scroll);
-        scroll.setBounds(20, 60, 455, 380);
-        scroll.setToolTipText("Sube y baja");
-
-//DETALLES
-//hacer que las palabras no se corten
-        txtarea.setLineWrap(true);
-        txtarea.setWrapStyleWord(true);
-//fuente        
-        Font fuente = new Font("Consolas", Font.BOLD, 30);
-        txtarea.setFont(fuente);
-
-//LABEL1
-        JLabel label1 = new JLabel("Zanoni-Moroncini ©");
-        frame.add(label1);
-        label1.setBounds(340, 350, 200, 200);
-        label1.setForeground(new Color(25, 114, 236));
-        label1.setToolTipText("Trabajo hecho por Zanoni y Moroncini");
-
-//LABEL2
-        JLabel label2 = new JLabel ("FUENTE");
-        frame.add(label2);
-        label2.setBounds(20, 3, 150, 25);
-        label2.setForeground(new Color(1));
-        label2.setToolTipText("Despliega el menu debajo para elegir un tipo de fuente");
+        area.setFont(new Font("Monospaced",Font.PLAIN,12));
+        JScrollPane scroll = new JScrollPane(area, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        add(scroll,BorderLayout.CENTER);
         
-//JComboBox
-        JComboBox combbox = new JComboBox();
-        frame.add(combbox);
-//    combbox.setBounds(X,Y,Widht,High);
-        combbox.setBounds(20, 25, 80, 20);
-        combbox.addItem("uno");
-        combbox.addItem("dos");
-        combbox.setToolTipText("Elige una fuente");
-
-        combbox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-
-            }
-        });
-
-//COLOR CHOOSER 
-        JButton btnColor = new JButton("Color de Fuente");
-        frame.add(btnColor);
-        btnColor.setBounds(110, 25, 125, 20);
-        btnColor.setToolTipText("Clickea para desplegar la paleta de colores");
+        JMenuBar JMB = new JMenuBar();
+        setJMenuBar(JMB);
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
+        JMB.add(file);
+        JMB.add(edit);
         
-        JColorChooser colores = new JColorChooser();
-//        frame.add(colores);
-//    combbox.setBounds(X,Y,Widht,High);
-    combbox.setBounds(20,20,100,20);
-    combbox.addItem("uno");
-    combbox.addItem("dos");
-       
-   combbox.addActionListener(new ActionListener() {
-   @Override
-   public void actionPerformed(ActionEvent ae) {
-       
-   }
-});  
-               
-        colores.setBounds(60, 200, 100, 20);
-        colores.setToolTipText("Elige un color de fuente");
-//Hacer la ventana/frame visible.
-        frame.setSize(500, 500);
-        frame.repaint();
-        frame.setVisible(true);
-    
+        file.add(New);
+        file.add(Open);
+        file.add(Save);
+        file.add(Quit);
+        file.add(SaveAs);
+        file.addSeparator();
+        
+        for(int i=0;i<4;i++)
+            file.getItem(i).setIcon(null);
+        
+        edit.add(Cut);
+        edit.add(Copy);
+        edit.add(Paste);
+        
+        edit.getItem(0).setText("Cut out");
+        edit.getItem(1).setText("Copy");
+        edit.getItem(2).setText("Paste");
+        
+        JToolBar tool = new JToolBar();
+        add(tool,BorderLayout.NORTH);
+        tool.add(New);
+        tool.add(Open);
+        tool.add(Save);
+        tool.addSeparator();
+        
+        JButton cut = tool.add(Cut), cop = tool.add(Copy), pas = tool.add(Paste);
+        cut.setText(null); cut.setIcon(new ImageIcon(""));
+        cop.setText(null); cop.setIcon(new ImageIcon(""));
+        pas.setText(null); pas.setIcon(new ImageIcon(""));
+        
+        Save.setEnabled(false);
+        SaveAs.setEnabled(false);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        area.addKeyListener(k1);
+        setTitle(currentFile);
+        setVisible(true);
     }
+    private KeyListener k1 = new KeyAdapter(){
+            public void keyPressed(KeyEvent e){
+                changed = true;
+                Save.setEnabled(true);
+                SaveAs.setEnabled(true);
+            }
+         
+        
+        
+    };
+    Action Open = new AbstractAction("Open", new ImageIcon("open.gif")){
+        
+        public void actionPerformed(ActionEvent e){
+            saveOld();
+            if(dialog.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+                readInFile(dialog.getSelectedFile().getAbsolutePath());
+            }
+            SaveAs.setEnabled(true);
+        }
+    };
+    
+    Action Save = new AbstractAction("Save",new ImageIcon("")){
+        public void actionPerformed(ActionEvent e){
+            if(!currentFile.equals("Untitled"))
+                saveFile(currentFile);
+            else
+                saveFileAs();
+        }
+        
+    };
+    Action SaveAs = new AbstractAction("Save as..."){
+      public void actionPerformed(ActionEvent e){
+          saveFileAs();
+      }
+    };
+    Action Quit = new AbstractAction("Quit"){
+      public void actionPerformed(ActionEvent e){
+          saveOld();
+          System.exit(0);
+      }  
+    };
+    Action New = new AbstractAction("New", new ImageIcon("")){
+      public void actionPerformed(ActionEvent e){
+          saveOld();
+          area.setText("");
+          currentFile = "Untitled";
+          setTitle(currentFile);
+          changed = false;
+          Save.setEnabled(false);
+          SaveAs.setEnabled(false);
+      }  
+    };
+    ActionMap m = area.getActionMap();
+        Action Cut = m.get(DefaultEditorKit.cutAction);
+        Action Copy = m.get(DefaultEditorKit.copyAction);
+        Action Paste = m.get(DefaultEditorKit.pasteAction);
+    
+    private void saveFileAs()
+    {
+        if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
+            saveFile(dialog.getSelectedFile().getAbsolutePath());
+    }
+    private void saveOld()
+    {
+        if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
+            saveFile(dialog.getSelectedFile().getAbsolutePath());
+    }
+    private void readInFile(String fileName)
+    {
+        try{
+            FileReader r = new FileReader(fileName);
+            area.read(r,null);
+            r.close();
+            currentFile=fileName;
+            setTitle(currentFile);
+            changed = false;
+        }
+        catch(IOException e){
+            Toolkit.getDefaultToolkit().beep();
+            JOptionPane.showMessageDialog(this,"Editor can't find the file called "+fileName);
+            
+        }
+            
+    }
+    private void saveFile(String fileName){
+        try{
+            FileWriter w = new FileWriter(fileName);
+            area.write(w);
+            w.close();
+            currentFile = fileName;
+            setTitle(currentFile);
+            changed = false;
+            Save.setEnabled(false);
+        }
+        catch(IOException e){
+            
+        }
+    }
+
+        
 
     public static void main(String[] args) {
         new ProyectoProgramacion1();
