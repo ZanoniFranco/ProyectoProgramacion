c89883206c3cbc4e0f9bf43fb387cd429275beb4
ManuMoroncini <manuelmoroncini2@gmail.com>
2018-08-16 14:30:29 -0300

    3.6 new,open,save,saveAs andan bien

--- ProyectoProgramacion1/src/proyectoprogramacion1/ProyectoProgramacion1.java
+++ ProyectoProgramacion1/src/proyectoprogramacion1/ProyectoProgramacion1.java
@@ -1,6 +1,7 @@
 package proyectoprogramacion1;
 
 import java.awt.*;
+import static java.awt.Color.*;
 //import java.awt.Color;
 //import java.awt.Font;
 import java.awt.event.*;
@@ -26,22 +27,23 @@ import javax.swing.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import javax.swing.text.DefaultEditorKit;
 
-public class ProyectoProgramacion1 extends JFrame{
-    private JTextArea area = new JTextArea(20,80);
+public class ProyectoProgramacion1 extends JFrame {
+
+    private JTextArea area = new JTextArea(20, 80);
     private JFileChooser dialog = new JFileChooser(System.getProperty("user.dir"));
 
     private String currentFile = "EDITOR DE TEXTO";
 
     private boolean changed = false;
-    
-    public ProyectoProgramacion1(){
-    
-        area.setFont(new Font("Monospaced",Font.PLAIN,12));
+
+    public ProyectoProgramacion1() {
+
+        area.setFont(new Font("Monospaced", Font.PLAIN, 12));
         area.setToolTipText("Hoja");
         JScrollPane scroll = new JScrollPane(area, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         scroll.setToolTipText("Sube y baja");
-        add(scroll,BorderLayout.CENTER);
-        
+        add(scroll, BorderLayout.CENTER);
+
         JMenuBar JMB = new JMenuBar();
         setJMenuBar(JMB);
         JMenu file = new JMenu("Archivo");
@@ -51,179 +53,194 @@ public class ProyectoProgramacion1 extends JFrame{
         JMB.add(edit);
         file.setToolTipText("Archivo");
         edit.setToolTipText("Editar");
-        
+
         file.add(New);
         file.add(Open);
         file.add(Save);
         file.add(Quit);
         file.add(SaveAs);
         file.addSeparator();
-        
-        for(int i=0;i<4;i++)
+
+        for (int i = 0; i < 4; i++) {
             file.getItem(i).setIcon(null);
-        
+        }
+
         edit.add(Cut);
         edit.add(Copy);
         edit.add(Paste);
-        
+
         edit.getItem(0).setText("Cortar");
+        edit.getItem(0).setToolTipText("Ctrl + X");
         edit.getItem(1).setText("Copiar");
+        edit.getItem(1).setToolTipText("Ctrl + C");
         edit.getItem(2).setText("Pegar");
-        
+        edit.getItem(2).setToolTipText("Ctrl + V");
+
         JToolBar tool = new JToolBar();
-        add(tool,BorderLayout.NORTH);
-        tool.add(New);
-        tool.add(Open);
-        tool.add(Save);
+        add(tool, BorderLayout.NORTH);
+//        tool.add(New);
+        tool.add(New).setToolTipText("Nuevo");
+//        tool.add(Open);
+        tool.add(Open).setToolTipText("Abrir");
+//        tool.add(Save);
+        tool.add(Save).setToolTipText("Guardar");
         tool.addSeparator();
-        
+
         JButton cut = tool.add(Cut), cop = tool.add(Copy), pas = tool.add(Paste);
-        cut.setText(null); cut.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\cortar.png"));
-        cop.setText(null); cop.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\copiar.png"));
-        pas.setText(null); pas.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\pegar.png"));
-        
+        cut.setToolTipText("Corta el texto seleccionado");
+        cop.setToolTipText("Copia el texto seleccionado");
+        pas.setToolTipText("Pega el texto que hay en el cortapapeles");
+        cut.setText(null);
+        cut.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\cortar.png"));
+        cop.setText(null);
+        cop.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\copiar.png"));
+        pas.setText(null);
+        pas.setIcon(new ImageIcon("C:\\Users\\Manuel\\Documents\\NetBeansProjects\\ProyectoProgramacion\\ProyectoProgramacion1\\src\\proyectoprogramacion1\\pegar.png"));
+
         Save.setEnabled(false);
         SaveAs.setEnabled(false);
-        
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         area.addKeyListener(k1);
         setTitle(currentFile);
         setVisible(true);
-      
-    //LABEL1
+
+        //LABEL1
         JLabel label1 = new JLabel("Zanoni&Moroncini ©");
         label1.setBounds(10, 30, 100, 30);
         label1.setForeground(new Color(25, 114, 236));
         label1.setToolTipText("Trabajo realizado por Moroncini y Zanoni");
         tool.add(label1);
-        
-    //COLOR CHOOSER 
+
+        //COLOR CHOOSER 
         JButton btnColor = new JButton("Color de Fuente");
         JMB.add(btnColor);
 //        btnColor.setBounds(10, 30, 125, 20);
         btnColor.setToolTipText("Clickea para desplegar la paleta de colores");
-        btnColor.addActionListener(new ActionListener(){
+        btnColor.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed (ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 //Abre la paleta de colores
-                Color colores=JColorChooser.showDialog(tool, "Eligir un color para la fuente", Color.BLACK);
+                Color colores = JColorChooser.showDialog(tool, "Eligir un color para la fuente", Color.BLACK);
                 //Cambia el color del boton para saber que color estamos usando
                 btnColor.setBackground(colores);
+//                btnColor.setForeground(WHITE);
                 area.setForeground(colores);
                 label1.setForeground(colores);
             }
         });
-        
+
     }
 
-    
-    private KeyListener k1 = new KeyAdapter(){
-            @Override
-            public void keyPressed(KeyEvent e){
-                changed = true;
-                Save.setEnabled(true);
-                SaveAs.setEnabled(true);
-            }
-         
-        
-        
-    };
-    Action Open = new AbstractAction("Abrir", new ImageIcon("open.gif")){
-        
+    private KeyListener k1 = new KeyAdapter() {
         @Override
-        public void actionPerformed(ActionEvent e){
-//            saveOld();
-            if(dialog.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+        public void keyPressed(KeyEvent e) {
+            changed = true;
+            Save.setEnabled(true);
+            SaveAs.setEnabled(true);
+        }
+
+    };
+    Action Open = new AbstractAction("Abrir", new ImageIcon("open.gif")) {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveOld();
+            if (dialog.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
                 readInFile(dialog.getSelectedFile().getAbsolutePath());
             }
             SaveAs.setEnabled(true);
         }
     };
-    
-    Action Save = new AbstractAction("Guardar",new ImageIcon("")){
+
+    Action Save = new AbstractAction("Guardar", new ImageIcon("")) {
         @Override
-        public void actionPerformed(ActionEvent e){
-            if(!currentFile.equals("Untitled"))
+        public void actionPerformed(ActionEvent e) {
+            if (!currentFile.equals("Untitled")) {
                 saveFile(currentFile);
-            else
+            } else {
                 saveFileAs();
+            }
         }
-        
+
     };
-    Action SaveAs = new AbstractAction("Guardar como..."){
-      @Override
-      public void actionPerformed(ActionEvent e){
-          saveFileAs();
-      }
+    Action SaveAs = new AbstractAction("Guardar como...") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveFileAs();
+        }
     };
-    Action Quit = new AbstractAction("Salir"){
-      @Override
-      public void actionPerformed(ActionEvent e){
-          saveOld();
-          System.exit(0);
-      }  
+    Action Quit = new AbstractAction("Salir") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveOld();
+            System.exit(0);
+        }
     };
-    Action New = new AbstractAction("Nuevo", new ImageIcon("")){
-      @Override
-      public void actionPerformed(ActionEvent e){
-          saveOld();
-          area.setText("");
-          currentFile = "Untitled";
-          setTitle(currentFile);
-          changed = false;
-          Save.setEnabled(false);
-          SaveAs.setEnabled(false);
-      }  
+    Action New = new AbstractAction("Nuevo", new ImageIcon("")) {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (saveOld()) {
+                area.setText("");
+                currentFile = "Untitled";
+                setTitle(currentFile);
+                changed = false;
+                Save.setEnabled(false);
+                SaveAs.setEnabled(false);
+            }
+
+        }
     };
     ActionMap m = area.getActionMap();
-        Action Cut = m.get(DefaultEditorKit.cutAction);
-        Action Copy = m.get(DefaultEditorKit.copyAction);
-        Action Paste = m.get(DefaultEditorKit.pasteAction);
-    
-    private void saveFileAs()
-    {
-        if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
+    Action Cut = m.get(DefaultEditorKit.cutAction);
+    Action Copy = m.get(DefaultEditorKit.copyAction);
+    Action Paste = m.get(DefaultEditorKit.pasteAction);
+
+    private void saveFileAs() {
+        if (dialog.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
             saveFile(dialog.getSelectedFile().getAbsolutePath());
+        }
     }
-    private void saveOld()
-    {
-        if(dialog.showSaveDialog(null)==JFileChooser.APPROVE_OPTION)
+
+    private Boolean saveOld() {
+        if (dialog.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
             saveFile(dialog.getSelectedFile().getAbsolutePath());
+            return true;
+        }
+        return false;
+
     }
-    private void readInFile(String fileName)
-    {
-        try{
+
+    private void readInFile(String fileName) {
+        try {
             FileReader r = new FileReader(fileName);
-            area.read(r,null);
+            area.read(r, null);
             r.close();
-            currentFile=fileName;
+            currentFile = fileName;
             setTitle(currentFile);
             changed = false;
-        }
-        catch(IOException e){
+        } catch (IOException e) {
             Toolkit.getDefaultToolkit().beep();
-            JOptionPane.showMessageDialog(this,"El editor no pudo encontrar el archivo: "+fileName);
-            
+            JOptionPane.showMessageDialog(this, "El editor no pudo encontrar el archivo: " + fileName);
+
         }
-            
+
     }
-    private void saveFile(String fileName){
-        try{
+
+    private void saveFile(String fileName) {
+        try {
             FileWriter w = new FileWriter(fileName);
             area.write(w);
             w.close();
             currentFile = fileName;
             setTitle(currentFile);
             changed = false;
-            Save.setEnabled(false);
-        }
-        catch(IOException e){
-            
+            Save.setEnabled(true);
+        } catch (IOException e) {
+
         }
     }
-
-        
 
     public static void main(String[] args) {
         new ProyectoProgramacion1();
